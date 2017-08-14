62c999d02c58ce06d48c3a2115d2ff6d692471d9
None <mr.flatiron@mail.ru>
2017-08-14 12:25:42 +0300

    names changed

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ int main (int argc, char *argv[])
   employee_list global_employee_list;
 
   employee_salesman emp_s;
-  emp_s.set_first_name ("Slava");
-  emp_s.set_last_name ("Nazarenko");
+  emp_s.set_first_name ("ab");
+  emp_s.set_last_name ("tt");
   emp_s.set_ssn (ssn_handle  ("123456789"));
   emp_s.set_salary (250000);
   emp_s.set_bonus_coef (0.15);
@@ -23,22 +23,22 @@ int main (int argc, char *argv[])
   emp_s.set_realized_outcome (100000);
 
   employee_hourly emp_h;
-  emp_h.set_first_name ("Darya");
-  emp_h.set_last_name ("Brusova");
+  emp_h.set_first_name ("cd");
+  emp_h.set_last_name ("qq");
   emp_h.set_ssn (ssn_handle ("987654321"));
   emp_h.set_wage (203240);
   emp_h.set_hours_done (10);
 
   employee_hourly emp_h1;
-  emp_h1.set_first_name ("Darya");
-  emp_h1.set_last_name ("Brusova");
+  emp_h1.set_first_name ("ef");
+  emp_h1.set_last_name ("tt");
   emp_h1.set_ssn (ssn_handle ("111111111"));
   emp_h1.set_wage (203240);
   emp_h1.set_hours_done (10);
 
   employee_hourly emp_h2;
-  emp_h2.set_first_name ("Darya");
-  emp_h2.set_last_name ("Brusova");
+  emp_h2.set_first_name ("gh");
+  emp_h2.set_last_name ("qq");
   emp_h2.set_ssn (ssn_handle ("222222222"));
   emp_h2.set_wage (203240);
   emp_h2.set_hours_done (10);
